7bc940de2564510b0119eb36e2ddf06d09002786
Lars Rosenkilde <bytesenseidk@gmail.com>
2024-12-27 21:25:28 +0100

    Fixed functionality by using yt-dlp instead of youtube-dl

--- Tmp_fix.py
+++
@@ -1,4 +0,0 @@
-import os
-
-def update():
-    os.system("python3 -m pip install --force-reinstall https://github.com/yt-dlp/yt-dlp/archive/master.tar.gz")
--- YoutubeDownloader.py
+++ YoutubeDownloader.py
@@ -2,7 +2,7 @@ from __future__ import unicode_literals
 import os
 import sys
 import threading
-import youtube_dl
+from yt_dlp import YoutubeDL
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.uic import loadUi
 from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog
@@ -19,92 +19,77 @@ class MainWindow(QMainWindow):
         self.button_download.clicked.connect(self.download_button)
         self.input_path.setText(self.std_download_path)
         self.radio_single.setChecked(True)
-        
-        
+
     def set_button(self):
         file_name = QFileDialog.getExistingDirectory()
         if file_name:
             self.input_path.setText(file_name)
 
-
     def download_button(self):
         url = self.input_url.text()
         save_path = self.input_path.text()
         quality = self.combo_quality.currentText()
-        download = threading.Thread(target=self.download_thread, args=(url, save_path, quality), daemon=True)
+        download = threading.Thread(
+            target=self.download_thread, args=(url, save_path, quality), daemon=True
+        )
         download.start()
 
-
     def download_thread(self, url, save_path, quality):
-        playlist = True
-        video_format = False
-        if self.radio_single.isChecked():
-            playlist = False
-        if self.check_video.isChecked():
-            video_format = True
+        playlist = not self.radio_single.isChecked()
+        video_format = self.check_video.isChecked()
 
         try:
-            Download(url, save_path, quality, video_format, playlist).download()
+            downloader = Download(url, save_path, quality, video_format, playlist)
+            downloader.download()
             self.input_url.setText("")
             self.label_done.setText("Download Done!")
         except Exception as E:
             self.input_url.setText("")
             self.label_done.setText(str(E))
-        
-        
+
+
 class Download(object):
     def __init__(self, url, save_path, quality, video_format, playlist=False):
         self.url = url
         self.save_path = save_path
-        self.qualities = {"Best": "1411",
-                          "Semi": "320",
-                          "Worst": "128"}
+        self.qualities = {"Best": "320", "Semi": "192", "Worst": "128"}
         self.video_format = video_format
-        self.quality = self.qualities[quality]
+        self.quality = self.qualities.get(quality, "192")
         self.playlist = playlist
-
 
     @property
     def video_opts(self):
         return {
             "verbose": False,
-            "fixup"  : "detect_or_warn",
-            "format" : "bestaudio/best",
-            "postprocessors" : [{
-                "key": "FFmpegVideoConverter",
-                "preferredcodec"  : "mp4",
-            }],
-            "outtmpl"     : self.save_path + "/%(title)s.%(ext)s",
-            "noplaylist"  : self.playlist
+            "format": "bestvideo+bestaudio/best",
+            "merge_output_format": "mp4",
+            "outtmpl": os.path.join(self.save_path, "%(title)s.%(ext)s"),
+            "noplaylist": not self.playlist,
         }
-    
+
     @property
     def song_opts(self):
         return {
             "verbose": False,
-            "fixup"  : "detect_or_warn",
-            "format" : "bestaudio/best",
-            "postprocessors" : [{
-                "key": "FFmpegExtractAudio",
-                "preferredcodec"  : "mp3",
-                "preferredquality": self.quality
-            }],
-            "extractaudio": True,
-            "outtmpl"     : self.save_path + "/%(title)s.%(ext)s",
-            "noplaylist"  : self.playlist
+            "format": "bestaudio/best",
+            "postprocessors": [
+                {
+                    "key": "FFmpegExtractAudio",
+                    "preferredcodec": "mp3",
+                    "preferredquality": self.quality,
+                }
+            ],
+            "outtmpl": os.path.join(self.save_path, "%(title)s.%(ext)s"),
+            "noplaylist": not self.playlist,
         }
-    
 
     def download(self):
-        if self.video_format:
-            download_object = youtube_dl.YoutubeDL(self.video_opts)
-        else:
-            download_object = youtube_dl.YoutubeDL(self.song_opts)
-        
-        info = download_object.extract_info(self.url, download=False)
-        song_name = info.get('title', None)
-        window.label_done.setText(song_name)
-        return download_object.download([self.url])
+        options = self.video_opts if self.video_format else self.song_opts
+        with YoutubeDL(options) as download_object:
+            info = download_object.extract_info(self.url, download=False)
+            song_name = info.get("title", None)
+            window.label_done.setText(song_name)
+            download_object.download([self.url])
 
 
 if __name__ == "__main__":
